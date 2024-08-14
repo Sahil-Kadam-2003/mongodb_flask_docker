2660e83712b47a2e105a53196d4dc8ae73b4176d
Sahil-Kadam-2003 <sahilkadam2205@gmail.com>
2024-08-14 14:52:31 +0530

    flask rest api connect mongodb and dockerise

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
-app = Flask('_name_')
+app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
 
-@app.route('/')
-def hello_world():
-    return hello_world
-
 @app.route('/add', methods=['POST'])
 def add_user():
-    user = request.json
-    mongo.db.users.insert_These({"name":"sahil","email":"sahilkadam2205@gmail.com"})
-    return add_user
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = list(mongo.db.users.find())
-    return get_users
     
-if __name__ == '__main__':
-    app.run(debug=True)
+    if request.is_json:
+        json = request.get_json()
+        name = json.get('name')
+        email = json.get('email')
+        pwd = json.get('pwd')
+
+        
+        if name and email and pwd:
+            mongo.db.user.insert_one({'name': name, 'email': email, 'pwd': pwd})
+            return jsonify({"message": "User added successfully"}), 201
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not Found"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
